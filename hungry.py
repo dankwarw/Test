4120583b4ddf5f399dc47aa136b720f8f24b3797
Richard Dankwa <rdankwa20@gmail.com>
2025-06-05 20:47:50 -0400

    Added Fufu

--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ hungry = input("are you hungry")
 if hungry == 'yes':
     print("eat samosa")
     print("eat pizza")
+    print("Fufu and Soup")
+    print("Burger")
 else:
     print(" do your homework")
