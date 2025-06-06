2463ce04ec48a90a5f5757ebd34fea138267912e
Richard Dankwa <rdankwa20@gmail.com>
2025-06-05 20:50:27 -0400

    Revert "Added Fufu"
    This reverts commit 4120583b4ddf5f399dc47aa136b720f8f24b3797.

--- hungry.py
+++ hungry.py
@@ -2,7 +2,5 @@ hungry = input("are you hungry")
 if hungry == 'yes':
     print("eat samosa")
     print("eat pizza")
-    print("Fufu and Soup")
-    print("Burger")
 else:
     print(" do your homework")
